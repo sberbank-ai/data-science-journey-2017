b3115a16f31c835b284388c1b7294a2b0313f7d7
Peter Romov <peter@romov.ru>
2017-10-07 02:03:55 +0300

    removed squad module

--- problem_B/check_solution.py
+++ problem_B/check_solution.py
@@ -1,25 +1,43 @@
+import re
 import sys
 import os
 import logging
 import zipfile
 import subprocess
 import json
+import tempfile
+from collections import Counter
+
+import tqdm
 import pandas
 import numpy as np
 import pandas as pd
-import tempfile
-import tqdm
-
 
 logger = logging.getLogger(__name__)
 PAR_ID = 'paragraph_id'
 Q_ID = 'question_id'
 
 
-def quality(data_file, prediction_file):
-    from squad import f1_score
+def normalize_answer(text):
+    """Lower text and remove punctuation and extra whitespace."""
+    return ' '.join(re.findall(r"\w+", text)).lower()
 
-    df_solution = pandas.read_csv(data_file, index_col=[PAR_ID,  Q_ID])['answer']
+
+def f1_score(prediction, ground_truth):
+    prediction_tokens = normalize_answer(prediction).split()
+    ground_truth_tokens = normalize_answer(ground_truth).split()
+    common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
+    num_same = sum(common.values())
+    if num_same == 0:
+        return 0
+    precision = 1.0 * num_same / len(prediction_tokens)
+    recall = 1.0 * num_same / len(ground_truth_tokens)
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+
+def quality(data_file, prediction_file):
+    df_solution = pandas.read_csv(data_file, index_col=[PAR_ID, Q_ID])['answer']
     df_predictions = pandas.read_csv(prediction_file, index_col=[PAR_ID, Q_ID])['answer']
 
     df_solution, df_predictions = df_solution.align(df_predictions, join='left')
@@ -30,24 +48,10 @@ def quality(data_file, prediction_file):
     return score
 
 
-# def quality(data_file, prediction_file):
-#     from squad import f1_score
-#
-#     df_solutions = pandas.read_csv(data_file, index_col=[PAR_ID,  Q_ID])[['answer', 'split']]
-#     df_predictions = pandas.read_csv(prediction_file, index_col=[PAR_ID, Q_ID])['answer']
-#
-#     r = []
-#     for f, n in [(lambda x: x, 'all'), (lambda x: x[x.split == 0], '0'), (lambda x: x[x.split == 1], '1')]:
-#         df_solution, df_prediction = f(df_solutions).answer.align(df_predictions, join='left')
-#         df_prediction = df_prediction.fillna('')
-#         x = np.mean([f1_score(pred, gt) for gt, pred in zip(df_solution.values, df_prediction.values)])
-#         r.append((n, x))
-#     return dict(r)
-
-
 def create_file_without_answer_column(data_file, data_file_without_answer):
     data = pd.read_csv(data_file)
-    data[["paragraph_id", "question_id", "paragraph", "question"]].to_csv(data_file_without_answer, header=True, index=False)
+    data[["paragraph_id", "question_id", "paragraph", "question"]].to_csv(data_file_without_answer, header=True,
+                                                                          index=False)
     del data
 
 
@@ -62,13 +66,11 @@ def get_submission_folder(submission_file, tmpdirname, submission_folder):
 
 
 class DockerEvaluator(object):
-
     def evaluate(self, data_file, submission_file=None, submission_folder=None, **kwargs):
         import docker
         # URGENT: it works well with native and VirtualBox docker daemon, remote cases doesnot work, but you can fix it
         docker_client = docker.from_env()
         with tempfile.TemporaryDirectory() as tmpdirname:
-            # TODO: build image
             image = kwargs.get('image', 'sberbank/sdsj-python')
             with tempfile.TemporaryDirectory() as tmpdirname:
                 prediction_file = os.path.join(tmpdirname, "result.csv")
@@ -156,7 +158,6 @@ class DockerEvaluator(object):
 
 
 class SimplePythonEvaluator(object):
-
     def evaluate(self, data_file, submission_file=None, submission_folder=None, **kwargs):
         if submission_file is None and submission_folder is None:
             raise Exception("one of submission_file or submission_folder should not be None")
@@ -180,6 +181,7 @@ class SimplePythonEvaluator(object):
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser(description='test submission')
     parser.add_argument('-z', '--submission_file', default=None, action='store', help='zip archive')
     parser.add_argument('-f', '--submission_folder', default=None, action='store', help='folder with submission files')
@@ -188,6 +190,7 @@ if __name__ == '__main__':
     evaluators = {'simple': SimplePythonEvaluator(), 'docker': DockerEvaluator()}
     args = parser.parse_args()
     import logging
+
     print(args)
     FORMAT = '%(asctime)-15s %(message)s'
     logging.basicConfig(format=FORMAT, level=logging.DEBUG)
